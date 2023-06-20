29dd27f6c04763e7da0299ec379570df3e790ab5
sunilkushwaha <sunilkushwaha055>
2023-06-20 19:22:02 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
+
+  const [count, setCount] = useState(0);
+
+  const [data]= useState(props.slides);
+
   return (
-    <></>
+    <>
+    <div>
+      <h1 data-testid="title">{data[count].title}</h1>
+      <p data-testid="text">{data[count].text}</p>
+      <button data-testid="button-next" disabled={count === props.slides.length-1 ? true : false} onClick={()=>setCount(count+1)}>Next</button>
+      <button data-testid="button-prev" disabled={count === 0 ? true : false} onClick={()=> setCount(count-1)}>Prev</button>
+      <button data-testid="button-restart" onClick={()=> setCount(0)} disabled={count === 0 ? true : false}>Restart</button>
+    </div>
+    </>
   )
 }
 
